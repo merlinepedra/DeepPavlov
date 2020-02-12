06d362df2a6ffc8e77be3a2f3cc85c15329d01ef
AlexeySorokin <alexey.sorokin@list.ru>
2020-02-12 17:10:38 +0300

    wip: tagger

--- deeppavlov/models/morpho_tagger/lemmatizer.py
+++ deeppavlov/models/morpho_tagger/lemmatizer.py
@@ -21,7 +21,7 @@ from russian_tagsets import converters
 
 from deeppavlov.core.common.registry import register
 from deeppavlov.core.models.serializable import Serializable
-from deeppavlov.models.morpho_tagger.common_tagger import get_tag_distance
+from deeppavlov.models.morpho_tagger.common_tagger import get_tag_distance, make_full_UD_tag, make_pos_and_tag
 
 
 class BasicLemmatizer(Serializable):
@@ -79,12 +79,21 @@ class UDPymorphyLemmatizer(BasicLemmatizer):
     """
 
     def __init__(self, save_path: Optional[str] = None, load_path: Optional[str] = None,
-                 transform_lemmas=False, **kwargs) -> None:
+                 transform_lemmas=False, check_proper_nouns=False, **kwargs) -> None:
         self.transform_lemmas = transform_lemmas
+        self.check_proper_nouns = check_proper_nouns
         self._reset()
         self.analyzer = MorphAnalyzer()
         self.converter = converters.converter("opencorpora-int", "ud20")
         super().__init__(save_path, load_path, **kwargs)
+
+    def _process_lemma(self, lemma, tag, word):
+        if "PROPN" in tag:
+            if word.isupper():
+                lemma = lemma.upper()
+            elif word[0].isupper():
+                lemma = lemma[0].upper() + lemma[1:]
+        return lemma
 
     def save(self, *args, **kwargs):
         pass
@@ -105,8 +114,37 @@ class UDPymorphyLemmatizer(BasicLemmatizer):
             curr_tag, curr_lemma = self.converter(str(parse.tag)), parse.normal_form
             distance = get_tag_distance(tag, curr_tag)
             if distance < best_distance:
-                best_lemma, best_distance = curr_lemma, distance
+                best_lemma, best_parse, best_distance = curr_lemma, parse, distance
                 if distance == 0:
                     break
+        # if word.isupper() and len(word) > 1:
+        #     best_lemma = best_lemma.upper()
+        # elif word[0].isupper():
+        #     best_lemma = best_lemma[0].upper() + best_lemma[1:]
+        best_lemma = self._process_lemma(best_lemma, tag, word)
         self.memo[(word, tag)] = best_lemma
         return best_lemma
+
+NUM_MAPPING = {"пять": "пятый", "четыре": "четвертый", "шесть": "шестой", "десять": "десятый"}
+
+@register("morphorueval_lemmatizer")
+class MorphoRuEvalLemmatizer(UDPymorphyLemmatizer):
+
+    def _process_lemma(self, lemma, tag, word):
+        # числительные
+        if lemma == "два" and word.lower().startswith("втор"):
+            lemma = "второй"
+        elif lemma == "три" and word.lower().startswith("трет"):
+            lemma = "третий" 
+        elif lemma == "один" and word.lower().startswith("перв"):
+            lemma = "первый"
+        elif lemma in NUM_MAPPING and "ADJ" in tag:
+            lemma = NUM_MAPPING[lemma]
+        # предлоги
+        elif word in ["об", "во", "со"] and "ADP" in tag:
+            lemma = word
+        # наречия
+        elif lemma in ["основный"]:
+            lemma = "основной"
+        lemma = UDPymorphyLemmatizer._process_lemma(self, lemma, tag, word)
+        return lemma
