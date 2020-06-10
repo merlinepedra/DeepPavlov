e0f5fed42f1722f45de889ed9cb32fc43f81a77c
Дмитрий Евсеев <dmitrij.euseew@yandex.ru>
2020-06-10 18:15:26 +0300

    function for building of inverted index and entity ranking by context and description

--- deeppavlov/models/kbqa/entity_linking.py
+++ deeppavlov/models/kbqa/entity_linking.py
@@ -14,17 +14,23 @@
 
 from logging import getLogger
 from typing import List, Dict, Tuple, Optional, Any
+from collections import defaultdict
 
+import re
 import nltk
 import pymorphy2
+import sqlite3
+from nltk.corpus import stopwords
 from rapidfuzz import fuzz
+from hdt import HDTDocument
 
 from deeppavlov.core.common.registry import register
 from deeppavlov.core.models.component import Component
 from deeppavlov.core.models.serializable import Serializable
-from deeppavlov.core.common.file import load_pickle
+from deeppavlov.core.commands.utils import expand_path
+from deeppavlov.core.common.file import load_pickle, save_pickle
 from deeppavlov.models.spelling_correction.levenshtein.levenshtein_searcher import LevenshteinSearcher
-from deeppavlov.models.kbqa.wiki_parser import WikiParser
+from deeppavlov.models.kbqa.rel_ranking_bert_infer import RelRankerBertInfer
 
 log = getLogger(__name__)
 
@@ -41,11 +47,21 @@ class EntityLinker(Serializable):
     """
 
     def __init__(self, load_path: str,
+                 save_path: str,
                  inverted_index_filename: str,
                  entities_list_filename: str,
                  q2name_filename: str,
-                 wiki_parser: WikiParser = None,
-                 use_hdt: bool = False,
+                 rel_ranker: RelRankerBertInfer = None,
+                 build_inverted_index: bool = False,
+                 kb_format: str = "hdt",
+                 kb_filename: str = None,
+                 label_rel: str = None,
+                 descr_rel: str = None,
+                 aliases_rels: List[str] = None,
+                 sql_table_name: str = None,
+                 sql_column_names: List[str] = None,
+                 lang: str = "en",
+                 use_descriptions: False,
                  lemmatize: bool = False,
                  use_prefix_tree: bool = False,
                  **kwargs) -> None:
@@ -62,18 +78,30 @@ class EntityLinker(Serializable):
             use_prefix_tree: whether to use prefix tree for search of entities with typos in entity labels
             **kwargs:
         """
-        super().__init__(save_path=None, load_path=load_path)
+        super().__init__(save_path=save_path, load_path=load_path)
         self.morph = pymorphy2.MorphAnalyzer()
         self.lemmatize = lemmatize
         self.use_prefix_tree = use_prefix_tree
-        self.use_hdt = use_hdt
-
         self.inverted_index_filename = inverted_index_filename
         self.entities_list_filename = entities_list_filename
+        self.build_inverted_index = build_inverted_index
         self.q2name_filename = q2name_filename
+        self.kb_format = kb_format
+        self.kb_filename = kb_filename
+        self.label_rel = label_rel
+        self.aliases_rels = aliases_rels
+        self.descr_rel = descr_rel
+        self.sql_table_name = sql_table_name
+        self.sql_column_names = sql_column_names
         self.inverted_index: Optional[Dict[str, List[Tuple[str]]]] = None
-        if self.use_hdt:
-            self.wiki_parser = wiki_parser
+        self.entities_index: Optional[List[str]] = None
+        self.q2name: Optional[List[Tuple[str]]] = None
+        self.stopwords = set(stopwords.words("english"))
+        self.re_tokenizer = re.compile(r"[\w']+|[^\w ]")
+        self.rel_ranker = rel_ranker
+        self.lang_str = f"@{lang}"
+        self.use_descriptions = use_descriptions
+        
         self.load()
 
         if self.use_prefix_tree:
@@ -83,26 +111,42 @@ class EntityLinker(Serializable):
             dictionary_words = list(self.inverted_index.keys())
             self.searcher = LevenshteinSearcher(alphabet, dictionary_words)
 
+        if self.build_inverted_index:
+            if self.kb_format == "hdt":
+                self.doc = HDTDocument(str(expand_path(self.kb_filename)))
+            if self.kb_format == "sqlite3":
+                self.conn = sqlite3.connect(str(expand_path(self.kb_filename)))
+                self.cursor = self.conn.cursor()
+
     def load(self) -> None:
         self.inverted_index = load_pickle(self.load_path / self.inverted_index_filename)
         self.entities_list = load_pickle(self.load_path / self.entities_list_filename)
-        if not self.use_hdt:
-            self.q2name = load_pickle(self.load_path / self.q2name_filename)
+        self.q2name = load_pickle(self.load_path / self.q2name_filename)
 
     def save(self) -> None:
-        pass
+        if self.build_inverted_index:
+            build_inverted_index()
 
-    def __call__(self, entity: str) -> Tuple[List[str], List[float]]:
+    def __call__(self, entities: List[str], entities_positions: List[List[int]] = None,
+                       context_tokens: List[str] = None) -> Tuple[List[List[str]], List[List[float]]]:
+        entities_list = []
+        for entity, entity_pos in zip(entities, entities_positions):
+            context = ""
+            if self.use_descriptions:
+                context = ' '.join(context_tokens[:entity_pos[0]]+["[ENT]"]+context_tokens[entity_pos[-1]+1:])
+            entities_ids, confidences = link_entity(entity, context)
+
+    def link_entity(self, entity: str, context: str = None):
         confidences = []
         if not entity:
-            wiki_entities = ['None']
+            entities_ids = ['None']
         else:
             candidate_entities = self.candidate_entities_inverted_index(entity)
             candidate_entities, candidate_names = self.candidate_entities_names(entity, candidate_entities)
-            wiki_entities, confidences, srtd_cand_ent = self.sort_found_entities(candidate_entities,
-                                                                                 candidate_names, entity)
+            entities_ids, confidences, srtd_cand_ent = self.sort_found_entities(candidate_entities,
+                                                                                 candidate_names, entity, context)
 
-        return wiki_entities, confidences
+        return entities_ids, confidences
 
     def candidate_entities_inverted_index(self, entity: str) -> List[Tuple[Any, Any, Any]]:
         word_tokens = nltk.word_tokenize(entity.lower())
@@ -133,20 +177,26 @@ class EntityLinker(Serializable):
 
     def sort_found_entities(self, candidate_entities: List[Tuple[int, str, int]],
                             candidate_names: List[List[str]],
-                            entity: str) -> Tuple[List[str], List[float], List[Tuple[str, str, int, int]]]:
+                            entity: str, context: str = None) -> Tuple[List[str], List[float], List[Tuple[str, str, int, int]]]:
         entities_ratios = []
         for candidate, entity_names in zip(candidate_entities, candidate_names):
-            entity_id = candidate[1]
-            num_rels = candidate[2]
-            entity_name = entity_names[0]
+            entity_num, entity_id, num_rels = candidate
             fuzz_ratio = max([fuzz.ratio(name, entity) for name in entity_names])
-            entities_ratios.append((entity_name, entity_id, fuzz_ratio, num_rels))
+            entities_ratios.append((entity_num, entity_id, fuzz_ratio, num_rels))
 
         srtd_with_ratios = sorted(entities_ratios, key=lambda x: (x[2], x[3]), reverse=True)
-        wiki_entities = [ent[1] for ent in srtd_with_ratios]
-        confidences = [float(ent[2]) * 0.01 for ent in srtd_with_ratios]
+        if self.use_descriptions:
+            num_to_id = {entity_num: entity_id for entity_num, entity_id, _, _ in srtd_with_ratios[:30]}
+            entity_numbers = [entity_num for entity_num, _, _, _ in srtd_with_ratios[:30]]
+            scores = self.rel_ranker.rank_rels(context, entity_numbers)
+            top_rels = [score[0] for score in scores]
+            entity_ids = [num_to_id[num] for num in top_rels]
+            confidences = [score[1] for score in scores]
+        else:
+            entity_ids = [ent[1] for ent in srtd_with_ratios]
+            confidences = [float(ent[2]) * 0.01 for ent in srtd_with_ratios]
 
-        return wiki_entities, confidences, srtd_with_ratios
+        return entity_ids confidences, srtd_with_ratios
 
     def candidate_entities_names(self, entity: str,
           candidate_entities: List[Tuple[int, str, int]]) -> Tuple[List[Tuple[int, str, int]], List[List[str]]]:
@@ -157,24 +207,76 @@ class EntityLinker(Serializable):
             entity_num = candidate[0]
             entity_id = candidate[1]
             entity_names = []
-            if self.use_hdt:
-                entity_name = self.wiki_parser("objects", "forw", entity_id, find_label=True)
-                if entity_name != "Not Found" and len(entity_name) < 2 * entity_length:
-                    entity_names.append(entity_name)
-                    aliases = self.wiki_parser("objects", "forw", entity_id, find_alias=True)
-                    for alias in aliases:
+            
+            entity_names_found = self.q2name[entity_num]
+            if len(entity_names_found[0]) < 6 * entity_length:
+                entity_name = entity_names_found[0]
+                entity_names.append(entity_name)
+                if len(entity_names_found) > 1:
+                    for alias in entity_names_found[1:]:
                         entity_names.append(alias)
-                    candidate_names.append(entity_names)
-                    candidate_entities_filter.append(candidate)
-            else:
-                entity_names_found = self.q2name[entity_num]
-                if len(entity_names_found[0]) < 6 * entity_length:
-                    entity_name = entity_names_found[0]
-                    entity_names.append(entity_name)
-                    if len(entity_names_found) > 1:
-                        for alias in entity_names_found[1:]:
-                            entity_names.append(alias)
-                    candidate_names.append(entity_names)
-                    candidate_entities_filter.append(candidate)
+                candidate_names.append(entity_names)
+                candidate_entities_filter.append(candidate)
 
         return candidate_entities_filter, candidate_names
+
+    def build_inverted_index(self):
+        entities_set = set()
+        id_to_label_dict = defaultdict(list)
+        id_to_descr_dict = {}
+        label_to_id_dict = {}
+        label_triplets = []
+        alias_triplets_list = []
+        descr_triplets = []
+        if self.kb_format == "hdt":
+            label_triplets, c = self.doc.search_triples("", self.label_rel, "")
+            for alias_rel in self.aliases_rels:
+                alias_triplets, c = self.doc.search_triples("", alias_rel, "")
+                alias_triplets_list.append(alias_triplets)
+            descr_triplets, c = self.doc.search_triples("", self.descr_rel, "")
+
+        if self.kb_format == "sqlite3":
+            subject, relation, obj = self.sql_column_names
+            query = f'SELECT {subject}, {relation}, {obj} FROM {self.sql_table_name} WHERE {relation} = "{self.label_rel}";'
+            res = self.cursor.execute(query)
+            label_triplets = res.fetchall()
+            for alias_rel in self.aliases_rels:
+                query = f'SELECT {subject}, {relation}, {obj} FROM {self.sql_table_name} WHERE {relation} = "{alias_rel}";'
+                res = self.cursor.execute(query)
+                alias_triplets = res.fetchall()
+                alias_triplets_list.append(alias_triplets)
+            query = f'SELECT {subject}, {relation}, {obj} FROM {self.sql_table_name} WHERE {relation} = "{self.descr_rel}";'
+            res = self.cursor.execute(query)
+            descr_triplets = res.fetchall()
+
+        for triplets in [label_triplets] + alias_triplets_list:
+            for triplet in alias_triplets:
+                entities_set.add(triplet[0])
+                if triplet[2].endswith(self.lang_str):
+                    label = triplet[2].replace(self.lang_str, '').replace('"', '')
+                    id_to_label_dict[triplet[0]].append(label)
+                    label_to_id_dict[label] = triplet[0]
+
+        for triplet in descr_triplets:
+            entities_set.add(triplet[0])
+            if triplet[2].endswith(self.lang_str):
+                descr = triplet[2].replace(self.lang_str, '').replace('"', '')
+                id_to_descr_dict[triplet[0]].append(descr)
+
+        entities_dict = {entity: n for n, entity in enumerate(entities_set)}
+            
+        inverted_index = defaultdict(list)
+        for label in label_to_id_dict:
+            tokens = re.findall(self.re_tokenizer, label)
+            for tok in tokens:
+                if len(tok) > 1 and tok not in self.stopwords:
+                    inverted_index[tok].append(entities_dict[label_to_id_dict[label]])
+        inverted_index = dict(inverted_index)
+        entities_list = list(entities_set)
+        labels_list = [id_to_label_dict[entity] for entity in entities_list]
+        descr_list = [id_to_descr_dict[entity] for entity in entities_list]
+
+        save_pickle(inverted_index, self.save_path / self.inverted_index_filename)
+        save_pickle(entities_list, self.save_path / self.entities_list_filename)
+        save_pickle(labels_list, self.save_path / self.q2name_filename)
+        save_pickle(descr_list, self.save_path / self.q2descr_filename)
