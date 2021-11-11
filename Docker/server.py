179a3e102330ac1c444274670fe366c0de7227ee
Дмитрий Евсеев <dmitrij.euseew@yandex.ru>
2021-11-11 11:48:38 +0300

    update

--- Docker/server.py
+++ Docker/server.py
@@ -11,7 +11,8 @@ from fastapi import HTTPException
 from starlette.middleware.cors import CORSMiddleware
 from starlette.requests import Request
 from starlette.responses import HTMLResponse
-from deeppavlov import configs, build_model
+from deeppavlov.core.commands.utils import parse_config
+from deeppavlov import configs, build_model, train_model, evaluate_model
 
 logger = getLogger(__file__)
 app = FastAPI()
@@ -25,7 +26,9 @@ app.add_middleware(
     allow_headers=['*']
 )
 
-ner = build_model(configs.ner.ner_rus_vx_distil, download=False)
+ner_config = parse_config("ner_rus_distilbert_torch.json")
+entity_detection_config = parse_config("ner_rus_vx_distil.json")
+entity_detection = build_model(entity_detection_config, download=False)
 
 @app.post("/model")
 async def model(request: Request):
@@ -33,14 +36,72 @@ async def model(request: Request):
         try:
             inp = await request.json()
             texts = inp["x"]
-            res = ner(texts)
-            logger.warning(f"res {res}")
-            logger.info(f"res {res}")
+            entity_substr, entity_lemm_substr, entity_offsets, entity_init_offsets, tags, sentences_offsets, \
+                sentences, probas = ner(texts)
+            res = {"entity_substr": entity_substr, "entity_lemm_substr": entity_lemm_substr,
+                   "entity_offsets": entity_offsets, "entity_init_offsets": entity_init_offsets, "tags": tags,
+                   "sentences_offsets": sentences_offsets, "sentences": sentences, "probas": probas}
+            return res
             
         except aiohttp.client_exceptions.ClientConnectorError:
             logger.warning(f'{host} is unavailable, restarting worker container')
             loop = asyncio.get_event_loop()
             loop.create_task(porter.update_container(host))
 
+@app.post("/train")
+async def model(request: Request):
+    while True:
+        try:
+            inp = await request.json()
+            train_filename = inp["train_filename"]
+            with open(train_filename, 'r') as fl:
+                total_data = json.load(fl)
+            train_data = total_data[:int(len(train_filename) * 0.9)]
+            test_data = total_data[int(len(train_filename) * 0.9):]
+            new_filename = f"{train_filename.strip('.json')}_train.json"
+            with open(new_filename, 'w', encoding="utf8") as out:
+                json.dump({"train": train_data, "valid": test_data, "test": test_data},
+                          out, indent=2, ensure_ascii=False)
+            
+            ner_config["dataset_reader"] = {
+                "class_name": "sq_reader",
+                "data_path": new_filename
+            }
+            new_config["metadata"]["MODEL_PATH"] = "{MODELS_PATH}/ner_rus_distilbert_torch_new"
+            train_model(new_config)
+            
+            return {"trained": True}
+            
+        except aiohttp.client_exceptions.ClientConnectorError:
+            logger.warning(f'{host} is unavailable, restarting worker container')
+            loop = asyncio.get_event_loop()
+            loop.create_task(porter.update_container(host))
+
+@app.post("/test")
+async def model(request: Request):
+    while True:
+        try:
+            inp = await request.json()
+            test_filename = inp["test_filename"]
+            with open(test_filename, 'r') as fl:
+                test_data = json.load(fl)
+            new_filename = f"{train_filename.strip('.json')}_test.json"
+            with open(new_filename, 'w', encoding="utf8") as out:
+                json.dump({"train": [], "valid": [], "test": test_data},
+                          out, indent=2, ensure_ascii=False)
+            
+            ner_config["dataset_reader"] = {
+                "class_name": "sq_reader",
+                "data_path": new_filename
+            }
+            res = evaluate_model(ner_config)
+            metrics = list(res["test"])
+            
+            return {"metrics": metrics}
+            
+        except aiohttp.client_exceptions.ClientConnectorError:
+            logger.warning(f'{host} is unavailable, restarting worker container')
+            loop = asyncio.get_event_loop()
+            loop.create_task(porter.update_container(host))
 
 uvicorn.run(app, host='0.0.0.0', port=8000)
