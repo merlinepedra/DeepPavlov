c4e40bbffa671d275684c1acc92a4802251b9515
Дмитрий Евсеев <dmitrij.euseew@yandex.ru>
2021-11-13 10:39:46 +0300

    update

--- Docker/server.py
+++ Docker/server.py
@@ -144,7 +144,7 @@ async def model_testing(request: Request):
             
             cur_metrics = dict(res["test"])
             logger.warning(f"cur_metrics {cur_metrics}")
-            cur_ner_f1 = cur_metrics["metrics"]["ner_f1"]
+            cur_ner_f1 = cur_metrics["ner_f1"]
             
             if Path(metrics_filename).exists():
                 df = pd.read_csv(metrics_filename)
